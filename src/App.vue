9257d5754ddab179732a1c40504e563d2611396f
skybainiao <chen2468830035@gmail.com>
2022-05-21 22:34:38 +0200

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="https://www.meishujixun.com/uploads/03ce09f04af9c2d5b82e79fd41318af5.jpg">
+    <img class="ig" alt="Vue logo" src="https://www.meishujixun.com/uploads/03ce09f04af9c2d5b82e79fd41318af5.jpg">
     <HelloWorld message="Chen&Wan blog"/>
   </div>
 </template>
@@ -24,5 +24,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.ig {
+  height:400px;
+ 
+
 }
 </style>
